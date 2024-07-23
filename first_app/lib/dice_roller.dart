4c2e034b18da101261a573fac78eb60129096075
渡邉友樹 <>
2024-07-23 20:02:59 +0900

    fix: サイコロの出目を1-6のランダムになるようにする

--- first_app/lib/dice_roller.dart
+++ first_app/lib/dice_roller.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -10,10 +13,11 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/dice-images/dice-2.png';
+  var currentDiceRoll = 2;
+
   void rollDice() {
     setState(() {
-      activeDiceImage = 'assets/dice-images/dice-4.png';
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -23,7 +27,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/dice-images/dice-$currentDiceRoll.png',
           width: 200,
         ),
         TextButton(
