532e82d08e0bee6493e6cc88d43ff84693f927d4
渡邉友樹 <hogendan@gmail.com>
2024-07-23 13:41:30 +0900

    refactor: ファイルに摘出

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
@@ -8,33 +9,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.purpleAccent,
-            Colors.deepPurpleAccent,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Color.fromARGB(255, 61, 42, 228),
-            fontSize: 30,
-          ),
-        ),
-      ),
-    );
-  }
 }
