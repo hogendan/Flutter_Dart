904c0aec14005e94dff3005ac96c6e69fae2bfdf
渡邉友樹 <hogendan@gmail.com>
2024-07-23 17:00:14 +0900

    feat: Roll Dice ボタンをクリックするとサイコロ2 -> 4 にする
    - サイコロ表示部分を StatefulWidget にする
    - setState() を呼び出す

---
+++ first_app/lib/dice_roller.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/dice-images/dice-2.png';
+  void rollDice() {
+    setState(() {
+      activeDiceImage = 'assets/dice-images/dice-4.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDiceImage,
+          width: 200,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.all(20),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
