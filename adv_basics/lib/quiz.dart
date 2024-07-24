6dc143b8e6c54ae8366b36c8adeac78d8432b2e3
渡邉友樹 <>
2024-07-24 11:35:10 +0900

    refactor: 以下のリファクタを実施
    - start_screen から画面全体の背景設定を除外し、クイズロゴ・テキスト・ボタンのみの構成にする
    - quiz を新規作成し、スタート画面の役割を持たせる。これをmain から呼ぶようにする。
    - questions_screen の枠だけ生成

---
+++ adv_basics/lib/quiz.dart
@@ -0,0 +1,30 @@
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 76, 32, 199),
+              Color.fromARGB(255, 76, 32, 199)
+            ], begin: Alignment.topLeft),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -5,44 +5,36 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 76, 32, 199),
-          Color.fromARGB(255, 76, 32, 199)
-        ], begin: Alignment.topLeft),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
-            color: const Color.fromARGB(150, 255, 255, 255),
-            width: 500,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          color: const Color.fromARGB(150, 255, 255, 255),
+          width: 500,
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        const Text(
+          'Learn Flutter the fun way!',
+          style: TextStyle(
+            color: Color.fromARGB(255, 196, 176, 200),
+            fontSize: 20,
           ),
-          const SizedBox(
-            height: 50,
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        OutlinedButton.icon(
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+          icon: const Icon(Icons.arrow_right_alt),
+          label: const Text(
+            'Start Quiz',
           ),
-          const Text(
-            'Learn Flutter the fun way!',
-            style: TextStyle(
-              color: Color.fromARGB(255, 196, 176, 200),
-              fontSize: 20,
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          OutlinedButton.icon(
-            onPressed: () {},
-            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            icon: const Icon(Icons.arrow_right_alt),
-            label: const Text(
-              'Start Quiz',
-            ),
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
