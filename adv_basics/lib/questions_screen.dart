3facbc83f69bab37cd16c7fdeb9cf044f1760c41
渡邉友樹 <>
2024-07-24 17:09:12 +0900

    feat: 回答ボタンを配置。

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,3 +1,5 @@
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -12,6 +14,36 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text('hoge');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            answerText: 'Answer1',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer2',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer3',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
