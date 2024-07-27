54cf248d46eb19f2c823eb4bc01d294f363b08b2
渡邉友樹 <hogendan@gmail.com>
2024-07-27 15:38:41 +0900

    rfactor: 回答サマリをwidget化

--- adv_basics/lib/question_summary/questions_summary.dart
+++ adv_basics/lib/question_summary/questions_summary.dart
@@ -1,4 +1,4 @@
-import 'package:adv_basics/question_summary/question_identifier.dart';
+import 'package:adv_basics/question_summary/summary_item.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsSummary extends StatelessWidget {
@@ -12,46 +12,11 @@ class QuestionsSummary extends StatelessWidget {
       height: 300,
       child: SingleChildScrollView(
         child: Column(
-          children: summaryData.map((data) {
-            return Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                QuestionIdentifier(
-                  questionIndex: data['question_index'] as int,
-                  isCorrectAnswer:
-                      data['user_answer'] == data['correct_answer'],
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        style: const TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        data['question_text'] as String,
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        data['user_answer'] as String,
-                        style: const TextStyle(
-                            color: Color.fromARGB(255, 25, 132, 75)),
-                      ),
-                      Text(
-                          style: const TextStyle(
-                              color: Color.fromARGB(255, 213, 84, 19)),
-                          data['correct_answer'] as String),
-                    ],
-                  ),
-                )
-              ],
-            );
-          }).toList(),
+          children: summaryData.map(
+            (data) {
+              return SummaryItem(summaryData: data);
+            },
+          ).toList(),
         ),
       ),
     );
