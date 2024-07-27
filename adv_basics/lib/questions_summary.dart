63ddff95d68f555eefa4777366b7ed8c2a34fbd8
渡邉友樹 <>
2024-07-27 14:09:34 +0900

    fix: レイアウト調整
    - 質問番号の色を、正解・不正解で分ける
    - ユーザー回答、正解の色を分ける
    - 質問・回答を左寄せにする

--- adv_basics/lib/questions_summary.dart
+++ adv_basics/lib/questions_summary.dart
@@ -7,6 +7,13 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color getQuestionIndexColor(Map<String, Object> summaryData) {
+      if (summaryData['user_answer'] == summaryData['correct_answer']) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
     return SizedBox(
       height: 300,
       child: SingleChildScrollView(
@@ -14,16 +21,37 @@ class QuestionsSummary extends StatelessWidget {
           children: summaryData.map((data) {
             return Row(
               children: [
-                Text(((data['question_index'] as int) + 1).toString()),
+                Text(
+                  style: TextStyle(
+                    color: getQuestionIndexColor(data),
+                  ),
+                  ((data['question_index'] as int) + 1).toString(),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
                 Expanded(
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(data['question_text'] as String),
+                      Text(
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        data['question_text'] as String,
+                      ),
                       const SizedBox(
                         height: 5,
                       ),
-                      Text(data['user_answer'] as String),
-                      Text(data['correct_answer'] as String),
+                      Text(
+                        data['user_answer'] as String,
+                        style: const TextStyle(
+                            color: Color.fromARGB(255, 25, 132, 75)),
+                      ),
+                      Text(
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 213, 84, 19)),
+                          data['correct_answer'] as String),
                     ],
                   ),
                 )
