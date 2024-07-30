d709c413b75c80502c4372498abb0a53b4883abe
渡邉友樹 <hogendan@gmail.com>
2024-07-30 14:04:45 +0900

    feat: リストアイテムをスワイプで削除

--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -6,15 +6,43 @@ class ExpensesList extends StatelessWidget {
   const ExpensesList({
     super.key,
     required this.expenses,
+    required this.onRemoveExpense,
   });
 
   final List<Expense> expenses;
+  final void Function(Expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) => onRemoveExpense(expenses[index]),
+        background: Container(
+          color: Colors.red,
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Icon(Icons.delete),
+            ),
+          ),
+        ),
+        secondaryBackground: Container(
+          color: Colors.red,
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Icon(Icons.delete),
+            ),
+          ),
+        ),
+        child: ExpenseItem(
+          expenses[index],
+        ),
+      ),
     );
   }
 }
