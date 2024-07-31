84e745d618780f2ce034416dd2b1b2b77901ce70
渡邉友樹 <>
2024-07-31 14:19:12 +0900

    feat: 指定したカテゴリに絞った 経費をもつインスタンスを作成するコンストラクタファンクションを追加

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -38,3 +38,22 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    return expenses.fold(0, (prevValue, element) => prevValue + element.amount);
+  }
+}
