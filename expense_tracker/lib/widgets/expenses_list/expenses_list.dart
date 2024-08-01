61c4f9bfc5e9c779932d9554c913cae25cb4747a
渡邉友樹 <>
2024-08-01 11:13:29 +0900

    feat: チャートグラフ追加

--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
@@ -14,37 +15,44 @@ class ExpensesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: expenses.length,
-      itemBuilder: (ctx, index) => Dismissible(
-        key: ValueKey(expenses[index]),
-        onDismissed: (direction) => onRemoveExpense(expenses[index]),
-        background: Container(
-          color: Theme.of(context).colorScheme.error,
-          margin: EdgeInsets.symmetric(
-              horizontal: Theme.of(context).cardTheme.margin!.horizontal),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Icon(Icons.delete),
+    return Column(
+      children: [
+        Chart(expenses: expenses),
+        Expanded(
+          child: ListView.builder(
+            itemCount: expenses.length,
+            itemBuilder: (ctx, index) => Dismissible(
+              key: ValueKey(expenses[index]),
+              onDismissed: (direction) => onRemoveExpense(expenses[index]),
+              background: Container(
+                color: Theme.of(context).colorScheme.error,
+                margin: EdgeInsets.symmetric(
+                    horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Icon(Icons.delete),
+                  ),
+                ),
+              ),
+              secondaryBackground: Container(
+                color: Theme.of(context).colorScheme.error,
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Icon(Icons.delete),
+                  ),
+                ),
+              ),
+              child: ExpenseItem(
+                expenses[index],
+              ),
             ),
           ),
         ),
-        secondaryBackground: Container(
-          color: Theme.of(context).colorScheme.error,
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: Icon(Icons.delete),
-            ),
-          ),
-        ),
-        child: ExpenseItem(
-          expenses[index],
-        ),
-      ),
+      ],
     );
   }
 }
