f6e2e4321d235844e51522c62d789ad806b61410
渡邉友樹 <>
2024-08-01 14:07:05 +0900

    feat: ローテーションによりレイアウトを変える。

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -66,6 +67,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -87,14 +90,23 @@ class _ExpensesState extends State<Expenses> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          const Text('The chart'),
-          Expanded(
-            child: mainContent,
-          ),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            ),
     );
   }
 }
--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -15,44 +15,37 @@ class ExpensesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Chart(expenses: expenses),
-        Expanded(
-          child: ListView.builder(
-            itemCount: expenses.length,
-            itemBuilder: (ctx, index) => Dismissible(
-              key: ValueKey(expenses[index]),
-              onDismissed: (direction) => onRemoveExpense(expenses[index]),
-              background: Container(
-                color: Theme.of(context).colorScheme.error,
-                margin: EdgeInsets.symmetric(
-                    horizontal: Theme.of(context).cardTheme.margin!.horizontal),
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Icon(Icons.delete),
-                  ),
-                ),
-              ),
-              secondaryBackground: Container(
-                color: Theme.of(context).colorScheme.error,
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Icon(Icons.delete),
-                  ),
-                ),
-              ),
-              child: ExpenseItem(
-                expenses[index],
-              ),
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) => onRemoveExpense(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error,
+          margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Icon(Icons.delete),
             ),
           ),
         ),
-      ],
+        secondaryBackground: Container(
+          color: Theme.of(context).colorScheme.error,
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Icon(Icons.delete),
+            ),
+          ),
+        ),
+        child: ExpenseItem(
+          expenses[index],
+        ),
+      ),
     );
   }
 }
