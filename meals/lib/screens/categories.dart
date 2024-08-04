450d997a85b772d0c0debfd4d1ff6a865aff372f
渡邉友樹 <hogendan@gmail.com>
2024-08-04 15:30:23 +0900

    feat: お気に入りボタンを押すと、お気に入りに追加/削除をする

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 import '../models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  CategoriesScreen(this.onToggleFavorite, {super.key});
+
+  void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals = dummyMeals
@@ -18,6 +21,7 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
