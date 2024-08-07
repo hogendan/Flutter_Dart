7ddb39f7100f8ae7d4c9976913b90c60877c5d8e
渡邉友樹 <>
2024-08-07 15:29:23 +0900

    fix: Padding 値ではなく、SlideTransition を使用してアニメーションさせる

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -78,8 +78,16 @@ class _CategoriesScreenState extends State<CategoriesScreen>
             )
         ],
       ),
-      builder: (context, child) => Padding(
-        padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3), // x:0, y:30%
+          end: const Offset(0, 0), // x:0, y:0 元の位置に戻す
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
+        ),
         child: child,
       ),
     );
