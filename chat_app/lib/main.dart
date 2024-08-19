db849ed095f3c7fc287b4c5d61b2876226d04a09
渡邉友樹 <hogendan@gmail.com>
2024-08-19 11:46:36 +0900

    Create New Project: chat_app

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home: ...,
+    );
+  }
+}
