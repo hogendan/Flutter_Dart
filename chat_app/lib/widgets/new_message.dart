eb48754f2ae11820f61a83dc96f495223a377f49
渡邉友樹 <hogendan@gmail.com>
2024-08-22 10:55:34 +0900

    feat: チャットメッセージを Firestore に保存する

--- chat_app/lib/widgets/new_message.dart
+++ chat_app/lib/widgets/new_message.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class NewMessage extends StatefulWidget {
@@ -8,7 +11,7 @@ class NewMessage extends StatefulWidget {
 }
 
 class _NewMessageState extends State<NewMessage> {
-  var _messageController = TextEditingController();
+  final _messageController = TextEditingController();
 
   @override
   void dispose() {
@@ -16,14 +19,29 @@ class _NewMessageState extends State<NewMessage> {
     super.dispose();
   }
 
-  void _submitMessage() {
+  void _submitMessage() async {
     final enteredMessage = _messageController.text;
 
     if (enteredMessage.trim().isEmpty) {
       return;
     }
 
-    // send to Firebase
+    // 認証されていないユーザーはここまで到達できないので null はない。
+    final user = FirebaseAuth.instance.currentUser!;
+
+    final userData = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get();
+
+    // doc との違いは、IDを指定するか自動生成かの違い
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createAt': Timestamp.now(),
+      'userId': user.uid,
+      'usrname': userData.data()!['username'],
+      'userImage': userData.data()!['image_url'],
+    });
 
     _messageController.clear();
   }
